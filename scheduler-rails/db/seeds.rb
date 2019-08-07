5f0c46061e495b9c4c406da34ec6cd8952ff71d2
Jon <github email address>
2019-08-07 11:13:26 -0400

    added seed data

--- scheduler-rails/db/seeds.rb
+++ scheduler-rails/db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Schedule.create(day: 'Monday' , time: 'AM', covered: false)
+Schedule.create(day: 'Monday' , time: 'PM', covered: false)
+
+Schedule.create(day: 'Tuesday' , time: 'AM', covered: false)
+Schedule.create(day: 'Tuesday' , time: 'PM', covered: false)
+
+Schedule.create(day: 'Wednesday' , time: 'AM', covered: false)
+Schedule.create(day: 'Wednesday' , time: 'PM', covered: false)
+
+Schedule.create(day: 'Thursday' , time: 'AM', covered: false)
+Schedule.create(day: 'Thursday' , time: 'PM', covered: false)
+
+Schedule.create(day: 'Friday' , time: 'AM', covered: false)
+Schedule.create(day: 'Friday' , time: 'PM', covered: false)
+
+Schedule.create(day: 'Saturday' , time: 'AM', covered: false)
+Schedule.create(day: 'Saturday' , time: 'PM', covered: false)
+
+Schedule.create(day: 'Sunday' , time: 'AM', covered: false)
+Schedule.create(day: 'Sunday' , time: 'PM', covered: false)
