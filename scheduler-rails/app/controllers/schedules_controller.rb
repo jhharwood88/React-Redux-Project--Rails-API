bf974e2f56a6bdbc5e554ce2c2b0cb28f9444125
Jon <github email address>
2019-08-07 11:14:20 -0400

    added schedules controller

---
+++ scheduler-rails/app/controllers/schedules_controller.rb
@@ -0,0 +1,23 @@
+class SchedulesController < ApplicationController
+
+	def index
+		@shifts = Schedule.all
+		render json: @shifts
+	end
+
+	def show
+		@shift = Schedule.find_by(id: params[:id])
+		render json: @shift 
+	end
+
+	def create 
+		@shift = Schedule.new(schedule_params)
+		render json: @shift 
+	end
+
+	private
+		def schedule_params
+			params.require(:shift).permit(:day, :time, :covered)
+		end
+
+end
